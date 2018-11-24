c4909e83d7b2919630c747af1b3706eccfd20cc8
lixinthu <georgethrax@users.noreply.github.com>
2018-11-24 08:44:23 +0200

    Create indoorClimate.py

---
+++ innerClimate/indoorClimate.py
@@ -0,0 +1,50 @@
+import json
+import requests
+
+class IndoorClimate:
+
+	def __init__(self, key = "zZW3DPNfb23pxs2c19uLb5YKX8AHRRCJ3eOIFQPO"):
+		self.key = key
+
+	def getData(self, siteID, sensorID, timeDateFrom, timeDateTo):
+		key = self.key
+
+		header = {"x-api-key": key}
+		body = test_data = {"timeDateFrom": timeDateFrom, "timeDateTo": timeDateTo}
+
+		requestUrl = 'https://v0wwaqqnpa.execute-api.eu-west-1.amazonaws.com/V1/sites/%s/sensors/%s/indoorclimate' %(siteID, sensorID)
+
+		response = requests.post(requestUrl, headers = header,json = json.dumps(body))
+
+		res = response.json()
+
+		print(res)
+		
+		if response.ok:
+			return {
+				"temperature": res["data"]["indoorClimate"][0]["temperature"],
+				"humidity": res["data"]["indoorClimate"][0]["humidity"],
+				"co2": res["data"]["indoorClimate"][0]["co2"]
+			}
+		else:
+			return None
+	
+		
+	
+
+if __name__ == "__main__":
+	
+	key = "zZW3DPNfb23pxs2c19uLb5YKX8AHRRCJ3eOIFQPO"
+	
+	Obj = IndoorClimate(key)
+
+	siteID = "site_exp"
+	sensorID = "b1239623-62b1-4a45-bf5a-b8b62056e372"
+	timeDateFrom= "2018-01-08T00:58:00.589234Z"
+	timeDateTo = "2018-01-08T01:58:00.589234Z"
+
+	print(siteID, sensorID, timeDateFrom, timeDateTo)
+
+	bool_occupancy = Obj.getData(siteID, sensorID, timeDateFrom, timeDateTo)
+	print("bool_occupancy: ")
+	print(bool_occupancy)
